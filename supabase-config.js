e84d2f5b9d6f502950f4a072668f5aa6cd482d00
ultrapips256 <waffijovan@gmail.com>
2026-09-13 15:40:48 +0300

    Add Supabase configuration file

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://yjfkgqzkctzosxwyzlwi.supabase.co";
+const SUPABASE_KEY = "sb_publishable_3OX1Y3peYDGyNux387khwQ_iI0bdHg3";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
